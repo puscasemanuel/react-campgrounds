3836f1e98e091ee6fd33d1dd09ae8f2a84d6a624
Ema <puscasemanuel7@gmail.com>
2020-08-07 10:31:31 +0300

    Final build done

--- server/app.js
+++ server/app.js
@@ -190,11 +190,10 @@ app.post('/api/v1/login', authController.login);
 //   });
 // });
 
-//ONLY FOR PRODUCTION
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 8080;
 
